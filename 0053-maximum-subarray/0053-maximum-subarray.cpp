ffb819fe4708708668b7cc6df7a0e3801126a8eb
RICHA SINGH <rssinghricha92@gmail.com>
2022-12-31 18:30:56 +0530

    Time: 130 ms (73.78%), Space: 67.7 MB (55.89%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+         
+       int n=nums.size();
+        int ans=nums[0],i,j,sum=0;
+        for(i=0;i<n;i++){
+            sum+=nums[i];
+            ans=max(sum,ans);
+            sum=max(sum,0);
+        }
+        return ans;
+    
+        
+    
+        
+    }
+};
