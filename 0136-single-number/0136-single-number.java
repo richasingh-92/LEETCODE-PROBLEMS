4da5dd793ac57918d14d939f99a0e39c4920a22f
RICHA SINGH <rssinghricha92@gmail.com>
2023-01-03 01:06:15 +0530

    Time: 1 ms (100.00%), Space: 42.6 MB (75.40%) - LeetHub

---
+++ 0136-single-number/0136-single-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+
+        int output = nums[0];
+        for(int i=1;i<nums.length;i++){
+            output^=nums[i];
+        }
+        return output;
+    }
+}
+        
+// only xor operation within loop 
+    
