1333a0f7fc42f5c07050a7915e4341e5b079afb2
RICHA SINGH <rssinghricha92@gmail.com>
2023-03-22 00:00:05 +0530

    Time: 4 ms (57.99%), Space: 60.1 MB (27.27%) - LeetHub

---
+++ 2348-number-of-zero-filled-subarrays/2348-number-of-zero-filled-subarrays.java
@@ -0,0 +1,20 @@
+class Solution {
+    public long zeroFilledSubarray(int[] nums) {
+        
+        boolean flag = false;
+        for(int i:nums)
+            if(i==0) flag=true;
+        
+        if(!flag) return 0; //if there are no zeroes in nums
+        long incr=1, len=0;
+        for(int i : nums){
+            if(i != 0) incr=1; //Set incr to 1 again
+            else { //For continuous Zeroes
+                len += incr; //Adding incr to our answer
+                incr++; //Increasing the current amount of zeroes filled subarrays, i.e. 1+2+3+4+....
+            }
+        }
+        return len;
+    }
+        
+    }
