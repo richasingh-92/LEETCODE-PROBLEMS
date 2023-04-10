703cd44c5794af192cdfb266ba5d4206c5bb37de
RICHA SINGH <rssinghricha92@gmail.com>
2023-04-10 23:34:26 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (86.36%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.cpp
+++ 0709-to-lower-case/0709-to-lower-case.cpp
@@ -7,3 +7,13 @@ public:
         return s;
     }
 };
+/*class Solution {
+public:
+    string toLowerCase(string s) {
+        for(int i=0;i<s.size();i++){
+            if(s[i] >= 'A' && s[i] <= 'Z')
+            s[i] = s[i] - 'A' + 'a';
+        }
+        return s;
+    }
+};*/
