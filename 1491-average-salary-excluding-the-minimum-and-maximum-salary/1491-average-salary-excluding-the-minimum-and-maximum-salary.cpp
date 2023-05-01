a30f356e347617ea91b5b00d8ae90c9e1751e350
RICHA SINGH <rssinghricha92@gmail.com>
2023-05-01 22:32:41 +0530

    Time: 0 ms (100.00%), Space: 7.2 MB (53.11%) - LeetHub

--- 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
+++ 1491-average-salary-excluding-the-minimum-and-maximum-salary/1491-average-salary-excluding-the-minimum-and-maximum-salary.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
     double average(vector<int>& salary) {
-        int max=0;
-        int min=INT_MAX;
-        double average=0;
-        for(int i=0;i<salary.size();i++)
-        {
-            if(salary[i]>= max)
-            {
-                max=salary[i];      
-            }
-            if (salary[i]<=min)
-            {
-                min=salary[i];
-            }
-            average=average+salary[i];
-        }
-        
-        average=average-(max+min);
-        return average/(salary.size()-2);
-    }
-};
-/*
-class Solution {
-public:
-    double average(vector<int>& salary) {
         int n = salary.size();
         double ans=0;
         sort(salary.begin(),salary.end());
@@ -33,4 +9,4 @@ public:
         }
         return ans/(n-2);
     }
-};*/
+};
